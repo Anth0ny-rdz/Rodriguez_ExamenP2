88db54253f1dedaa59568d63cd9dfe5d7ccf99a6
Anthony Josué Rodríguez Camuendo <anthony.rodriguez.camuendo@udla.edu.ec>
2023-12-06 08:17:30 -0500

    Adicion de la logica para que ya funciones el boton calcular

--- Views/AnthonyRodriguez_Conteo.xaml.cs
+++ Views/AnthonyRodriguez_Conteo.xaml.cs
@@ -3,7 +3,53 @@ namespace Apuntes.Views;
 public partial class AnthonyRodriguez_Conteo : ContentPage
 {
 	public AnthonyRodriguez_Conteo()
-	{
-		InitializeComponent();
-	}
+    {
+        InitializeComponent();
+    }
+
+    private void Calcular_Clicked(object sender, EventArgs e)
+    {
+        string input = txtInput.Text;
+
+        int numbers = 0;
+        int letters = 0;
+        int vowels = 0;
+        int upperCase = 0;
+        int lowerCase = 0;
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+                numbers++;
+            else if (char.IsLetter(c))
+            {
+                letters++;
+                if ("aeiouAEIOU".Contains(c))
+                    vowels++;
+
+                if (char.IsUpper(c))
+                    upperCase++;
+                else
+                    lowerCase++;
+            }
+        }
+
+        lblNumbers.Text = $"Números: {numbers}";
+        lblLetters.Text = $"Letras: {letters}";
+        lblVowels.Text = $"Vocales: {vowels}";
+        lblUpperCase.Text = $"Mayúsculas: {upperCase}";
+        lblLowerCase.Text = $"Minúsculas: {lowerCase}";
+        lblTotal.Text = $"Total: {input.Length}";
+    }
+
+    private void Limpiar_Clicked(object sender, EventArgs e)
+    {
+        txtInput.Text = string.Empty;
+        lblNumbers.Text = string.Empty;
+        lblLetters.Text = string.Empty;
+        lblVowels.Text = string.Empty;
+        lblUpperCase.Text = string.Empty;
+        lblLowerCase.Text = string.Empty;
+        lblTotal.Text = string.Empty;
+    }
 }
