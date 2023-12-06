a30fca3395570a4087d85945f233082b126667c0
Anthony Josué Rodríguez Camuendo <anthony.rodriguez.camuendo@udla.edu.ec>
2023-12-06 07:50:50 -0500

    Adicion de los atribustos de la clase de conteo

--- Models/ANTHONYRodriguez_Conteo.cs
+++ Models/ANTHONYRodriguez_Conteo.cs
@@ -8,5 +8,12 @@ namespace Apuntes.Models
 {
     internal class ANTHONYRodriguez_Conteo
     {
+        public string Cadena { get; set; }
+        public int numeros { get; set; }
+        public int letras { get; set; }
+        public int vocales { get; set; }
+        public int mayusuculas { get; set; }
+        public int minusculas { get; set; }
     }
 }
+
